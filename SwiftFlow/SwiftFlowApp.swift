7fe6cb01805886a0f8a4d5ef5161da608cb2cc79
Felipe Gomez <fiatdv>
2022-05-09 10:49:08 -0400

    Initial Commit

---
+++ SwiftFlow/SwiftFlowApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftFlowApp.swift
+//  SwiftFlow
+//
+//  Created by Felipe on 5/9/22.
+//
+
+import SwiftUI
+
+@main
+struct SwiftFlowApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
